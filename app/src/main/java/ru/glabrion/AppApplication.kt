9369ddf69cf03f84eb7241220001a095e88cc9ca
Sergey Sulatskov <s.sulatskov@delaweb.ru>
2019-12-19 15:07:35 +0400

    init

--- app/src/main/java/ru/glabrion/AppApplication.kt
+++ app/src/main/java/ru/glabrion/AppApplication.kt
@@ -8,7 +8,6 @@ open class AppApplication() : Application() {
 
     override fun onCreate() {
         super.onCreate()
-
         startKoin {
             modules(mainModule(applicationContext))
         }
